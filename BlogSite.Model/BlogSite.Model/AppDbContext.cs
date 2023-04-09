550ef2f7309da9e75ae2f1b5286a2447adafac0a
Daniel Gray <grayd4@spu.edu>
2023-04-09 15:03:44 -0700

    Basic model and DB configuration

---
+++ BlogSite.Model/BlogSite.Model/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace BlogSite.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Text> Texts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Video> Videos { get; set; }
+    }
+}
---
+++ BlogSite.Model/BlogSite.Model/Image.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSite.Model
+{
+    public class Image
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Guid PostId { get; set; }
+        [Required]
+        public virtual Post Post { get; set; }
+        public string Caption { get; set; }
+    }
+}
